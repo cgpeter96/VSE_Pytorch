b0a1fd913a54c2d32c8fa7a35bea13ed2b97d8b9
chenguang <cgpeter96@users.noreply.github.com>
2018-03-13 16:41:25 +0800

    Update utils.py
    在pytorch后续版本 需要添加keepdim=True 才能保持shape不变

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def l2norm(input, p=2.0, dim=1, eps=1e-12):
     """
     Compute L2 norm, row-wise
     """
-    return input / input.norm(p, dim).clamp(min=eps).expand_as(input)
+    return input / input.norm(p, dim,keepdim=True).clamp(min=eps).expand_as(input)
 
 
 def xavier_weight(tensor):
